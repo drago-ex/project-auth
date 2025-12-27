5d574667fdd6d32618cf2b3d3368a779e3349253
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-12-27 18:07:05 +0100

    Update SignTemplate.php

--- resources/app/UI/Backend/Sign/SignTemplate.php
+++ resources/app/UI/Backend/Sign/SignTemplate.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\UI\Backend\Sign;
 
-use App\Core\User\User;
+use App\Core\User\UserAccess;
 use App\UI\BaseTemplate;
+use Nette\Security\User;
 
 
 final class SignTemplate extends BaseTemplate
@@ -14,6 +15,6 @@ final class SignTemplate extends BaseTemplate
 	 * The logged-in user.
 	 * This can be either the Nette User or a custom User class from App\Core\User.
 	 */
-	public \Nette\Security\User|User $user;
+	public UserAccess|User $user;
 	public SignRecoveryToken $signRecoveryToken;
 }
