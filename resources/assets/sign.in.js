6cacaafdfbad0abe7411be295914d7674a9209e7
accgit <zdenek.papucik@gmail.com>
2025-12-28 09:27:07 +0100

    dev

--- resources/assets/sign.in.js
+++ resources/assets/sign.in.js
@@ -1,2 +1,10 @@
 import "./base.js"; // Located in Drago Project
+import { PasswordToggle, SubmitButtonDisable } from "drago-form";
 import "./sign.in.scss";
+
+/* naja extensions */
+function registerExtensions(...extensions) {
+	extensions.forEach(ext => naja.registerExtension(new ext()));
+}
+
+registerExtensions(PasswordToggle, SubmitButtonDisable);
