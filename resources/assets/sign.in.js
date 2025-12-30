f0812d3697dab0c772a3e1e3b0643ac77a7143ee
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-12-30 15:50:28 +0100

    Update sign.in.js

--- resources/assets/sign.in.js
+++ resources/assets/sign.in.js
@@ -1,10 +1,22 @@
-import "./base.js"; // Located in Drago Project
+// these JS + SCSS will be automatically available after installing the package
+import "./core/base.js";
+import "./naja/naja.spinner.js";
+import "./naja/naja.spinner.scss";
+
+// drago-form extensions
 import { PasswordToggle, SubmitButtonDisable } from "drago-form";
+
+// page styles
 import "./sign.in.scss";
 
-/* naja extensions */
+// registration naja extensions
 function registerExtensions(...extensions) {
-	extensions.forEach(ext => naja.registerExtension(new ext()));
+	extensions.forEach(Extension => {
+		naja.registerExtension(new Extension());
+	});
 }
 
-registerExtensions(PasswordToggle, SubmitButtonDisable);
+registerExtensions(
+	PasswordToggle,
+	SubmitButtonDisable
+);
