56ca4aff5c8ec20948bdcf5ec075a3581b80af08
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-01-09 19:34:49 +0100

    Update UserRepository.php

--- resources/app/UI/Backend/Sign/User/UserRepository.php
+++ resources/app/UI/Backend/Sign/User/UserRepository.php
@@ -6,7 +6,6 @@ namespace App\UI\Backend\Sign\User;
 
 use Dibi\Connection;
 use Dibi\Exception;
-use Dibi\Result;
 use Drago\Attr\AttributeDetectionException;
 use Drago\Attr\Table;
 use Drago\Database\Database;
