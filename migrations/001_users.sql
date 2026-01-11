f1dd1bd92b5b8d60ec87338b87f00bc40ad9de15
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-01-11 18:46:17 +0100

    Update 001_users.sql

--- migrations/001_users.sql
+++ migrations/001_users.sql
@@ -9,6 +9,6 @@ CREATE TABLE users (
     token VARCHAR(32) NOT NULL,
     CONSTRAINT uq_users_email UNIQUE (email)
 )
-ENGINE = InnoDB
-DEFAULT CHARSET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
