5e91df3b2b34ad6b3c5e4996109cb7d4d5a3bc49
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-05 19:32:39 +0200

    Update and rename sign.in.js to sign-in.js

--- resources/assets/sign-in.js
+++ resources/assets/sign-in.js
@@ -1,15 +1,15 @@
 // these JS + SCSS will be automatically available after installing the package
 import "./core/base.js";
-import "./naja/naja.spinner.scss";
-import SpinnerExtension from "./naja/naja.spinner.js";
-import HyperlinkDisable from "./naja/naja.hyperlink.js";
+import "./naja/spinner.scss";
+import SpinnerExtension from "./naja/spinner.js";
+import HyperlinkDisable from "./naja/hyperlink-disable.js";
 
 // drago-form extensions
 import { PasswordToggle, SubmitButtonDisable } from "drago-form";
 import { ToastHandler } from "drago-application";
 
 // page styles
-import "./sign.in.scss";
+import "./sign-in.scss";
 
 // registration naja extensions
 function registerExtensions(...extensions) {
