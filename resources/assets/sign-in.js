ec36960556aa966d1666c8c5a090ce4748b1432e
accgit <zdenek.papucik@gmail.com>
2026-06-18 08:58:16 +0200

    Update

--- resources/assets/sign-in.js
+++ resources/assets/sign-in.js
@@ -2,7 +2,7 @@
 import { registerNajaExtensions } from "./core/base.js";
 import Spinner from "./naja/spinner.js";
 import HyperlinkDisable from "./naja/hyperlink-disable.js";
-import ThemeSwitcher from "theme-switcher-compostrap";
+import { ThemeSwitcher } from "theme-switcher-compostrap";
 
 // drago-ex extensions
 import { PasswordToggle, SubmitButtonDisable } from "drago-form";
